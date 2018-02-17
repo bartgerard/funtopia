fe637d6b16e4c8360a48de78add06ab5e1f8ceb5
bartgerard <bart.gerard@gmail.com>
2018-02-17 13:55:24 +0100

    start.spring.io Initialized

---
+++ src/main/java/be/gerard/funtopia/FuntopiaApplication.java
@@ -0,0 +1,12 @@
+package be.gerard.funtopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuntopiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuntopiaApplication.class, args);
+	}
+}
